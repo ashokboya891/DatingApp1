2007e4e1d90684011d97ac18e37f71485b742b9d
ashokboya891 <ashokboya891@gmail.com>
2023-08-06 21:58:38 +0530

    end of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -9,14 +9,14 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-
+builder.Services.AddCors();
 builder.Services.AddDbContext<DataContext>(opt=>{
 opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 var app = builder.Build();
 
 
- 
+ app.UseCors(builder=>builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
 app.MapControllers();
 
